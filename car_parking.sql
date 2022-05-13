0270ef3e3ea9ebdd208a332ac3f84c342522384a
Md. Minul Hasan Rokan <70903774+minulhasanrokan@users.noreply.github.com>
2022-05-13 12:25:19 +0600

    Add files via upload
    Database

---
+++ car_parking.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 13, 2022 at 08:24 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `car_parking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parking_rate`
+--
+-- Error reading structure for table car_parking.parking_rate: #1932 - Table 'car_parking.parking_rate' doesn't exist in engine
+-- Error reading data for table car_parking.parking_rate: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`parking_rate`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_company`
+--
+-- Error reading structure for table car_parking.tbl_company: #1932 - Table 'car_parking.tbl_company' doesn't exist in engine
+-- Error reading data for table car_parking.tbl_company: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`tbl_company`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_parking`
+--
+-- Error reading structure for table car_parking.tbl_parking: #1932 - Table 'car_parking.tbl_parking' doesn't exist in engine
+-- Error reading data for table car_parking.tbl_parking: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`tbl_parking`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_slots`
+--
+-- Error reading structure for table car_parking.tbl_slots: #1932 - Table 'car_parking.tbl_slots' doesn't exist in engine
+-- Error reading data for table car_parking.tbl_slots: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`tbl_slots`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+-- Error reading structure for table car_parking.tbl_user: #1932 - Table 'car_parking.tbl_user' doesn't exist in engine
+-- Error reading data for table car_parking.tbl_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`tbl_user`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table car_parking.users: #1932 - Table 'car_parking.users' doesn't exist in engine
+-- Error reading data for table car_parking.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_groups`
+--
+-- Error reading structure for table car_parking.user_groups: #1932 - Table 'car_parking.user_groups' doesn't exist in engine
+-- Error reading data for table car_parking.user_groups: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`user_groups`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vechile_category`
+--
+-- Error reading structure for table car_parking.vechile_category: #1932 - Table 'car_parking.vechile_category' doesn't exist in engine
+-- Error reading data for table car_parking.vechile_category: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `car_parking`.`vechile_category`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
